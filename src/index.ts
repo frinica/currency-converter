55076974e04f9e8790c6b0e40bbf22789938377a
frinica <you@example.com>
2023-10-10 16:40:39 +0200

    add get request for country data

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response, Application } from "express"
 import dotenv from "dotenv"
+import axios from "axios"
+import { CountryData } from "./types/countryData"
 
 dotenv.config({ path: "./src/config.env" })
 
@@ -8,7 +10,23 @@ const app: Application = express()
 const port = process.env.PORT || 8001
 
 app.get("/", (req: Request, res: Response): void => {
-  res.send("Hello from server!")
+  res.send("Server is running!")
+})
+
+// Get data about a country by name
+app.get("/country/:name", async (req: Request, res: Response) => {
+  const countryName = req.params.name
+  const countryData = await axios.get(
+    `https://restcountries.com/v3.1/name/${countryName}`
+  )
+  const filteredData: CountryData = {
+    fullName: countryData.data[0].name.official,
+    population: countryData.data[0].population,
+    currencies: countryData.data[0].currencies,
+    //exchangeRate: "exchange Rate",
+  }
+
+  res.send(filteredData)
 })
 
 app.listen(port, (): void => {
---
+++ src/types/countryData.ts
@@ -0,0 +1,5 @@
+export interface CountryData {
+  fullName: String
+  population: Number
+  currencies: {}
+}
