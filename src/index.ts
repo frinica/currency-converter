d046a41d5137bc42b3de12012fe28e2f529b1d7d
frinica <you@example.com>
2023-10-10 17:36:45 +0200

    add request to fixer api for latest exchange rates

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response, Application } from "express"
 import dotenv from "dotenv"
 import axios from "axios"
 import { CountryData } from "./types/countryData"
+import { ExchangeRates } from "./types/exchangeRates"
 
 dotenv.config({ path: "./src/config.env" })
 
@@ -13,7 +14,7 @@ app.get("/", (req: Request, res: Response): void => {
   res.send("Server is running!")
 })
 
-// Get data about a country by name
+// GET data about a country by name
 app.get("/country/:name", async (req: Request, res: Response) => {
   const countryName = req.params.name
   const countryData = await axios.get(
@@ -23,10 +24,25 @@ app.get("/country/:name", async (req: Request, res: Response) => {
     fullName: countryData.data[0].name.official,
     population: countryData.data[0].population,
     currencies: countryData.data[0].currencies,
-    //exchangeRate: "exchange Rate",
   }
 
   res.send(filteredData)
+})
+
+// GET latest exchange rates
+// Note: Param "base" is not customizable with the free plan
+app.get("/currency", async (req: Request, res: Response) => {
+  const exchangeRatesData = await axios.get("http://data.fixer.io/api/latest", {
+    params: {
+      access_key: process.env.FIXER_API_KEY,
+    },
+  })
+  const filteredExchangeRates: ExchangeRates = {
+    base: exchangeRatesData.data.base,
+    date: exchangeRatesData.data.date,
+    rates: exchangeRatesData.data.rates,
+  }
+  res.send(filteredExchangeRates)
 })
 
 app.listen(port, (): void => {
