7296bb7f73ac6b58ce051dc4eb57aa24870ffc67
frinica <you@example.com>
2023-10-12 13:58:50 +0200

    emit props from search to parent component home

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,11 +1,22 @@
+import { useState } from "react"
 import { Search } from "./Search"
 
 export const Home = ({ props }: { props: string }) => {
   const token = props
+  const [data, setData] = useState<Object[]>([])
+
+  const dataFromChild = (emittedData: Object) => {
+    if (data) {
+      setData((oldArr) => [...oldArr, emittedData])
+    } else {
+      setData([emittedData])
+    }
+  }
+
   return (
     <>
       <h1>Welcome!</h1>
-      <Search props={token} />
+      <Search props={token} dataFromChild={dataFromChild} />
     </>
   )
 }
